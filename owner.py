499b4b29ec949e7476ea631b4fc7c6b36cf11ee5
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2023-04-18 20:15:24 +0530

    data dump

--- owner.py
+++ owner.py
@@ -13,8 +13,13 @@ f1.write('user_id, restaurant_id\n')
 
 random_small_users = random.sample(range(1, 50000), 5000)
 
+st = set()
 for i in range(1, 100000):
    user_id_idx = random.randint(1, 5000)
    user_id = random_small_users[user_id_idx-1]
    restaurant_id = random.randint(1, 40227)
-   f1.write(str(user_id) + ',' + str(restaurant_id) + '\n')
+   st.add(str(user_id) + ',' + str(restaurant_id))
+
+for s in st:
+   f1.write(s + '\n')
+
--- user.py
+++ user.py
@@ -6,11 +6,20 @@ f1 = open(output_file, 'w', encoding='utf-8')
 s = "id,name,email,password\n"
 f1.write(s)
 
+def clean(s):
+   # make password alphanumeric
+   s2 = ""
+   for c in s:
+      if c.isalnum():
+         s2 += c
+   return s2
+
 id = 1
 with open('data.json') as f:
    data = json.load(f)
    for item in data:
-      s = str(id) + "," + item['name'] + "," + item['email'] + "," + item['password'] + "\n"
+      pwd = clean(item['password'])
+      s = str(id) + "," + item['name'] + "," + item['email'] + "," + pwd + "\n"
       f1.write(s)
       id += 1
 
